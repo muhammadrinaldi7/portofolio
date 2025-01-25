7d8d4b0a646d5f6bdba2c79d9b910b58d43fedf3
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2025-01-25 17:42:16 +0800

    Penambahan Project ShowCase

--- src/component/Card/CardProject.jsx
+++ src/component/Card/CardProject.jsx
@@ -1,13 +1,18 @@
 export const CardProject = (props) => {
-
-    const { image, title, project } = props
-    return (
-        <div className="flex flex-col p-3 bg-white rounded-lg shadow-md hover:scale-105 lg:hover:rotate-2 hover:shadow-2xl">
-            <div className="w-full h-64 mb-4 bg-gray-100 rounded-lg">
-                <img src={image} className="object-cover w-full h-full rounded-lg" alt="" />
-            </div>
-            <p className="font-sans text-sm font-bold text-gray-400">{title}</p>
-            <h1 className="text-xl font-medium">{project}</h1>
-        </div>
-    )
-}
+  const { image, title, project, links } = props;
+  return (
+    <div className="flex flex-col p-3 bg-white rounded-lg shadow-md hover:scale-105 lg:hover:rotate-2 hover:shadow-2xl">
+      <div className="w-full h-64 mb-4 bg-gray-100 rounded-lg">
+        <img
+          src={image}
+          className="object-cover w-full h-full rounded-lg"
+          alt=""
+        />
+      </div>
+      <p className="font-sans text-sm font-bold text-gray-400">{title}</p>
+      <a href={links} target="_blank" rel="noopener noreferrer">
+        <h1 className="text-xl font-medium">{project}</h1>
+      </a>
+    </div>
+  );
+};
--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -3,49 +3,119 @@ import Footer from "../../component/Footer";
 import Header from "../../component/Header";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 import { CardProject } from "../../component/Card/CardProject";
-import simpeg from "../../assets/img/simpeg.png"
-import absensi from "../../assets/img/absensi.png"
-import petshop from "../../assets/img/petshop.png"
-import bpn from "../../assets/img/bpn.png"
-import meatgenkz from "../../assets/img/meatgenkz.png"
-import nansarunai from "../../assets/img/nansarunai.png"
-import sijantan from "../../assets/img/sijantan.png"
-import sibetina from "../../assets/img/sibetina.png"
+import simpeg from "../../assets/img/simpeg.png";
+import absensi from "../../assets/img/absensi.png";
+import petshop from "../../assets/img/petshop.png";
+import bpn from "../../assets/img/bpn.png";
+import meatgenkz from "../../assets/img/meatgenkz.png";
+import nansarunai from "../../assets/img/nansarunai.png";
+import sijantan from "../../assets/img/sijantan.png";
+import sibetina from "../../assets/img/sibetina.png";
+import travelyou from "../../assets/img/travelyou.png";
+import api from "../../assets/img/api.png";
+import rentcar from "../../assets/img/rentcar.png";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 const Project = () => {
-    useEffect(() => {
-        Aos.init({
-            easing: "ease-out",
-            duration: 1500, // Durasi animasi (milidetik)
-            once: false, // Animasi hanya muncul sekali
-        });
-    }, []);
-    return (
-        <>
-            <Header />
-            <section className="container mx-auto">
-                <div className="flex items-center justify-center w-full py-8" data-aos="fade-up">
-                    <FontAwesomeIcon icon={faCode} className="text-green-500" size="2x"/>
-                    <h1 className="mx-4 text-2xl font-bold text-center uppercase lg:text-4xl">SHOW Project</h1>
-                    <FontAwesomeIcon icon={faCode} className="text-green-500" size="2x"/>
-                </div>
-                <div className="grid grid-cols-1 gap-8 p-2 lg:grid-cols-3" data-aos="zoom-in">
-                    <CardProject image={sijantan} title="React Js & Laravel" project="Aplikasi Monitoring Bayi Bebas Penyakit Jantung Bawaan" />
-                    <CardProject image={sibetina} title="React Js & Laravel" project="Aplikasi Monitoring Ibu Hamil Bebas Anemia" />
-                    <CardProject image={nansarunai} title="Html, css, js" project="Landing Page Nansarunai" />
-                    <CardProject image={meatgenkz} title="Html, css, js" project="Landing Page Meatgenkz" />
-                    <CardProject image={petshop} title="Laravel" project="Web Petshop" />
-                    <CardProject image={bpn} title="CodeIgniter" project="Sistem Informasi Permohonan Pengukuran" />
-                    <CardProject image={simpeg} title="CodeIgniter" project="Sistem Informasi Kepegawaian" />
-                    <CardProject image={absensi} title="CodeIgniter" project="Sistem Informasi Absensi" />
-                </div>
-            </section>
-            <Footer/>
-        </>
-    );
-}
+  useEffect(() => {
+    Aos.init({
+      easing: "ease-out",
+      duration: 1500, // Durasi animasi (milidetik)
+      once: false, // Animasi hanya muncul sekali
+    });
+  }, []);
+  return (
+    <>
+      <Header />
+      <section className="container mx-auto">
+        <div
+          className="flex items-center justify-center w-full py-8"
+          data-aos="fade-up"
+        >
+          <FontAwesomeIcon icon={faCode} className="text-green-500" size="2x" />
+          <h1 className="mx-4 text-2xl font-bold text-center uppercase lg:text-4xl">
+            SHOW Project
+          </h1>
+          <FontAwesomeIcon icon={faCode} className="text-green-500" size="2x" />
+        </div>
+        <div
+          className="grid grid-cols-1 gap-8 p-2 lg:grid-cols-3"
+          data-aos="zoom-in"
+        >
+          <CardProject
+            image={sijantan}
+            links="https://sipp.web.id/"
+            title="React Js & Laravel"
+            project="Aplikasi Monitoring Bayi Bebas Penyakit Jantung Bawaan"
+          />
+          <CardProject
+            image={sibetina}
+            links="https://temansetiaibuhamil.web.id/"
+            title="React Js & Laravel"
+            project="Aplikasi Monitoring Ibu Hamil Bebas Anemia"
+          />
+          <CardProject
+            image={nansarunai}
+            links="https://nansarunaimegavision.org/"
+            title="Html, css, js"
+            project="Landing Page Nansarunai"
+          />
+          <CardProject
+            image={meatgenkz}
+            links="https://meatgenkz.nansarunaimegavision.org/"
+            title="Html, css, js"
+            project="Landing Page Meatgenkz"
+          />
+          <CardProject
+            image={petshop}
+            links="#"
+            title="Laravel"
+            project="Web Petshop"
+          />
+          <CardProject
+            image={bpn}
+            links="#"
+            title="CodeIgniter"
+            project="Sistem Informasi Permohonan Pengukuran"
+          />
+          <CardProject
+            image={simpeg}
+            links="#"
+            title="CodeIgniter"
+            project="Sistem Informasi Kepegawaian"
+          />
+          <CardProject
+            image={absensi}
+            links="#"
+            title="CodeIgniter"
+            project="Sistem Informasi Absensi"
+          />
+          <CardProject
+            image={travelyou}
+            links="https://travelyou.vercel.app/"
+            title="Next JS"
+            project="Aplikasi Transaksi Travel"
+          />
+          <CardProject
+            image={api}
+            links="https://rent-car-api.temansetiaibuhamil.web.id/"
+            title="Laravel API"
+            project="Backend API App Rental Mobil"
+          />
 
-export default Project
+          <CardProject
+            image={rentcar}
+            links="https://rentalline.vercel.app/"
+            title="Next JS"
+            project="Web App Rental Mobil"
+          />
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default Project;
